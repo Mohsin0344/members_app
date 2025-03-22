c886583a0b48e94474779c0865e7b15736954e4e
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-23 00:25:03 +0500

    Added AbsentMembersViewModel
    Added AbsentMembersViewModel for managing paginated absence data with filtering and error handling

---
+++ lib/view_models/members/absent_members_view_model.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:members_app/models/members/absent_members_model.dart';
+import '../../api/api_client.dart';
+import '../../api/api_methods.dart';
+import '../../api/api_routes.dart';
+import '../../models/pagination_request_model.dart';
+import '../app_states.dart';
+import '../view_model_exception_handler.dart';
+
+class AbsentMembersViewModel extends Cubit<AppState>
+    with ExceptionHandlingMixin<AppState> {
+  AbsentMembersViewModel() : super(const InitialState());
+  var client = ApiClient();
+  final PagingController<int, AbsentMember> absentMembersPagingController =
+  PagingController(
+    firstPageKey: 1,
+  );
+
+  getAbsentMembers({required PaginationRequest paginationRequest}) async {
+    try {
+      emit(const LoadingState());
+      final model = await client.request<AbsentMembersModel>(
+        endpoint: ApiRoute.getAbsentMembersRoute.path,
+        method: ApiMethod.get.value,
+        model: AbsentMembersModel(),
+        queryParams: paginationRequest.toJson(),
+      );
+      emit(
+        SuccessState(
+          data: model,
+        ),
+      );
+    } catch (e) {
+      handleException(e);
+    }
+  }
+}
