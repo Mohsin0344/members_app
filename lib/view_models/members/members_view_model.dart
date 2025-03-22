aa6c9cdbef7b2c1341e628b87afeaf9152a53567
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-22 22:33:46 +0500

    Added MembersViewModel to fetch members

---
+++ lib/view_models/members/members_view_model.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import '../../api/api_client.dart';
+import '../../api/api_methods.dart';
+import '../../api/api_routes.dart';
+import '../../models/members/members_model.dart';
+import '../../models/pagination_request_model.dart';
+import '../app_states.dart';
+import '../view_model_exception_handler.dart';
+
+class MembersViewModel extends Cubit<AppState>
+    with ExceptionHandlingMixin<AppState> {
+  MembersViewModel() : super(const InitialState());
+  var client = ApiClient();
+  final PagingController<int, Member> membersPagingController =
+      PagingController(
+    firstPageKey: 1,
+  );
+
+  getTeamMembers({required PaginationRequest paginationRequest}) async {
+    try {
+      emit(const LoadingState());
+      final model = await client.request<MembersModel>(
+        endpoint: ApiRoute.getTeamMembersRoute.path,
+        method: ApiMethod.get.value,
+        model: MembersModel(),
+        queryParams: paginationRequest.toJson(),
+      );
+      emit(
+        SuccessState(
+          data: model,
+        ),
+      );
+    } catch (e) {
+      handleException(e);
+    }
+  }
+}
