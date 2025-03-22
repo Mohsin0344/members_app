a3fa4c3ffe9c1b1880b12d71cd1d4fd296b11689
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-22 22:36:30 +0500

    Added absent team members screen

---
+++ lib/views/members/absent_team_members_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AbsentTeamMembersScreen extends StatefulWidget {
+  const AbsentTeamMembersScreen({super.key});
+
+  @override
+  State<AbsentTeamMembersScreen> createState() => _AbsentTeamMembersScreenState();
+}
+
+class _AbsentTeamMembersScreenState extends State<AbsentTeamMembersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
