0fec4432593489e934f8d2e8ead968ec81408818
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-22 21:16:40 +0500

    Added sample BlocProvider for testing

--- lib/bloc_providers/bloc_providers.dart
+++ lib/bloc_providers/bloc_providers.dart
@@ -1,9 +1,12 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../view_models/test_view_model.dart';
+
 
 class BlocProviders {
   static final List<BlocProvider> providers = [
-
+    BlocProvider<TestViewModel>(
+      create: (context) => TestViewModel(),
+    ),
   ];
 }
