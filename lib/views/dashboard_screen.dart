11559ea16e2ab456f492e4b14481c3f61cfafc72
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-22 21:41:58 +0500

    Added bottom navigation bar with screens in dashboard

--- lib/views/dashboard_screen.dart
+++ lib/views/dashboard_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../utils/app_assets.dart';
+import '../utils/app_colors.dart';
+import 'members/absent_team_members_screen.dart';
+import 'members/team_members_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,8 +13,42 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const TeamMembersScreen(),
+    const AbsentTeamMembersScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Image.asset(AppAssets.appLogo,
+        width: 0.50.sw,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.secondaryColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Members',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.miscellaneous_services),
+            label: 'Absences',
+          ),
+        ],
+      ),
+    );
   }
 }
