49c7ac4b7e848ed4aac63ea614a2b2efa3bfa250
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-23 05:18:45 +0500

    Added BlocProvider for ICal generation

--- lib/bloc_providers/bloc_providers.dart
+++ lib/bloc_providers/bloc_providers.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:members_app/data/absence_ical_service.dart';
 
 import '../view_models/absences/absence_types_view_model.dart';
+import '../view_models/absences/absences_ical_view_model.dart';
 import '../view_models/members/absent_members_view_model.dart';
 import '../view_models/members/members_view_model.dart';
 import '../view_models/test_view_model.dart';
-
 
 class BlocProviders {
   static final List<BlocProvider> providers = [
@@ -19,6 +20,11 @@ class BlocProviders {
     ),
     BlocProvider<AbsenceTypesViewModel>(
       create: (context) => AbsenceTypesViewModel(),
-    )
+    ),
+    BlocProvider<AbsencesIcalViewModel>(
+      create: (context) => AbsencesIcalViewModel(
+        AbsenceIcalServiceRepository(),
+      ),
+    ),
   ];
 }
