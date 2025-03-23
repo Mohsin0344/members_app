b3f6eb4e59273296b2ebac2cc9bc5729cb212bf0
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-23 23:34:13 +0500

    Changed api config to prod

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   //prod url
-  // static String baseUrl = 'https://absence-management-api-production.up.railway.app/api';
+  static String baseUrl = 'https://absence-management-api-production.up.railway.app/api';
   //local url
-  static String baseUrl = 'http://192.168.18.6:5000/api';
+  // static String baseUrl = 'http://192.168.18.6:5000/api';
 }
